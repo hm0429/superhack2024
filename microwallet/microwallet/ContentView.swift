4963c4a3accf34c6774d23f94691e03de4883a85
Hideyoshi Moriya <hide@piyo.jp>
2024-08-11 06:11:38 +0900

    Update ContentView.swift

--- microwallet/microwallet/ContentView.swift
+++ microwallet/microwallet/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("address")
+            Text("balance")
+            Button {
+                
+            } label: {
+                Text("export")
+            }
         }
         .padding()
     }
