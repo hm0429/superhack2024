74f32ae0c68aba9584df785acb37afc822cee75a
Hideyoshi Moriya <hide@piyo.jp>
2024-08-11 06:08:33 +0900

    add a new ios project

---
+++ microwallet/microwallet/microwalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  microwalletApp.swift
+//  microwallet
+//
+//  Created by Hideyoshi Moriya on 2024/08/11.
+//
+
+import SwiftUI
+
+@main
+struct microwalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
