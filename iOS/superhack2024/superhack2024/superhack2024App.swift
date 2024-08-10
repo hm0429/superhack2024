a8b08d20f6f45f926ed4dbae4674a75a39589d48
Hideyoshi Moriya <hide@piyo.jp>
2024-08-11 05:52:36 +0900

    add a new iOS project

---
+++ iOS/superhack2024/superhack2024/superhack2024App.swift
@@ -0,0 +1,17 @@
+//
+//  superhack2024App.swift
+//  superhack2024
+//
+//  Created by Hideyoshi Moriya on 2024/08/11.
+//
+
+import SwiftUI
+
+@main
+struct superhack2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
